73f8e15462908b1eb586ffa07ad1e74aa4e77b31
zhengqiang <zq1024088055@163.com>
2025-05-15 19:25:20 +0800

    add:新增配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://sigam-zq.github.io',
+  	base: 'astro-blog',
 	integrations: [mdx(), sitemap()],
 });
