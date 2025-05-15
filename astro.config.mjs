cd1b5ddceb74e2c9fc4796318f5d37a42b0a65a8
zhengqiang <zq1024088055@163.com>
2025-05-15 20:00:03 +0800

    constom-cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sigam-zq.github.io',
-  	base: 'astro-blog',
+	site: 'https://blogs.zqsrh.com',
 	integrations: [mdx(), sitemap()],
 });
