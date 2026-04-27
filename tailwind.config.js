fec62d9bd718ae5d3851c97fea05f9f9e0c051c6
z-mbp <1024088055@qq.com>
2026-04-27 21:39:47 +0800

    feat:修改首页样式

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
